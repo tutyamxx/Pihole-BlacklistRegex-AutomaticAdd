f5f84ee61ac412a14513281347c65e01ed71216b
Alexandru Maxim <15928925+tutyamxx@users.noreply.github.com>
2020-01-22 04:19:09 +0000

    Updated

--- piregex.sh
+++ piregex.sh
@@ -2,7 +2,8 @@
 echo "[+] Purging the regex.list file..."
 pihole --regex --nuke 
 
-echo "[+] Adding regex to a fresh regex.list..."
-pihole --regex '^(.+[-_.])??ad[sxv]?[0-9]*[-_.]' '^(.+[-_.])??adse?rv(er?|ice)?s?[0-9]*[-.]' '^(.+[-_.])??m?ad[sxv]?[0-9]*[-_.]' '^(.+[-_.])??xn--' '^(.+[_.-])?ad[sxv]?[0-9]*[_.-]' '^(.+[_.-])?adse?rv(er?|ice)?s?[0-9]*[_.-]' '^(.+[_.-])?telemetry[_.-]' '^(www[0-9]*\.)?xn--' '^adim(age|g)s?[0-9]*[_.-]' '^adse?rv(e(rs?)?|ices?)?[0-9]*[-.]' '^adtrack(er|ing)?[0-9]*[_.-]' '^advert(s|is(ing|ements?))?[0-9]*[-_.]' '^advert(s|is(ing|ements?))?[0-9]*[_.-]' '^aff(iliat(es?|ion))?[-.]' '^aff(iliat(es?|ion))?[_.-]' '^analytics?[-.]' '^analytics?[_.-]' '^banners?[-.]' '^banners?[_.-]' '^beacons?[0-9]*[-.]' '^beacons?[0-9]*[_.-]' '^clicks?[-.]' '^count(ers?)?[0-9]*[-.]' '^count(ers?)?[0-9]*[_.-]' '^mads\.' '^stat(s|istics)?[0-9]*[-.]' '^stat(s|istics)?[0-9]*[_.-]' '^telemetry[-.]' '^track(ers?|ing)?[0-9]*[-.]' '^track(ers?|ing)?[0-9]*[_.-]' '^traff(ic)?[-.]' '^traff(ic)?[.-]'
+echo "[+] Adding a dank regex to a fresh regex.list..."
+pihole --regex '^(.+[-_.])??ad[sxv]?[0-9]*[-_.]' '^(.+[-_.])??adse?rv(er?|ice)?s?[0-9]*[-.]' '^(.+[-_.])??m?ad[sxv]?[0-9]*[-_.]' '^(.+[-_.])??xn--' '^(www[0-9]*\.)?xn--' '(^|\.)xn--.*$' '^(.+[_.-])?telemetry[_.-]' '^adse?rv(e(rs?)?|ices?)?[0-9]*[-.]' '^advert(s|is(ing|ements?))?[0-9]*[-_.]' '^adim(age|g)s?[0-9]*[_.-]' '^adtrack(er|ing)?[0-9]*[_.-]' '^aff(iliat(es?|ion))?[-.]' '^analytics?[-.]' '^banners?[-.]' '^mads\.' '(.*)\.g00\.(.*)' '^beacons?[0-9]*[-.]' '^clicks?[-.]' '^count(ers?)?[0-9]*[-.]' '^pixels?[-.]' '^stat(s|istics)?[0-9]*[-.]' '^telemetry[-.]' '^track(ers?|ing)?[0-9]*[-.]' '^traff(ic)?[-.]' '(ads|logs|cloudservices).roku.com$' '^([a-z0-9]+[.])*(ads|captive|cloudservices|logs)\.roku\.com' '(.*\.|^)((think)?with)?google($|((adservices|apis|mail|static|syndication|tagmanager|tagservices|usercontent|zip|-analytics)($|\..+)))' '(^|\.)buffpanel\.com$' '(^|\.)bugsnag\.com$' '(^|\.)redshell\.io$' '(^|\.)treasuredata\.com$' '(^|\.)unity(|3d)\.com$' '(^|\.)unityads(|\.co)\.com$'
 
-echo "[+] Done!"
+echo "[+] Done! Now restarting FTL service"
+sudo service pihole-FTL restart
